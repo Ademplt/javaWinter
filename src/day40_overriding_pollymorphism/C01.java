25cb9048ec4436da9bfa3ec01a2c26a2fc31ce45
Adem Polat <polatadem.2541@gmail.com>
2022-04-10 16:00:19 +0200

    guncel

---
+++ src/day40_overriding_pollymorphism/C01.java
@@ -0,0 +1,14 @@
+package day40_overriding_pollymorphism;
+
+public class C01 {
+    public void method1(){
+        System.out.println("parent method1 calisti");
+    }
+    public void method2(){
+        System.out.println("parent method2 ");
+    }
+protected String method3(){
+        return "java";
+}
+
+}
