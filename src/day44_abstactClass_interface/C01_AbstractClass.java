3993ffb0720dd07197d87b63cfed26f901fbc455
Adem Polat <polatadem.2541@gmail.com>
2022-04-16 11:13:03 +0200

    16.04.2022

---
+++ src/day44_abstactClass_interface/C01_AbstractClass.java
@@ -0,0 +1,8 @@
+package day44_abstactClass_interface;
+
+public abstract class C01_AbstractClass {
+    public abstract void motor();
+    public abstract void vites();
+    public void teker(){
+    }
+}
