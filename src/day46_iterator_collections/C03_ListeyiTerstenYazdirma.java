c298071e53c525ebac3e439181a36e826a7e11b6
Adem Polat <polatadem.2541@gmail.com>
2022-04-17 12:28:53 +0200

    17.04.2022

--- src/day46_iterator_collections/C03_ListeyiTerstenYazdirma.java
+++ src/day46_iterator_collections/C03_ListeyiTerstenYazdirma.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.ListIterator;
 
 public class C03_ListeyiTerstenYazdirma {
+    // verilen bir listeyi, iterator ile sondan basa dogru yazdiralim
     public static void main(String[] args) {
         List<Integer> liste= new ArrayList<>();
         liste.add(5);
@@ -12,15 +13,14 @@ public class C03_ListeyiTerstenYazdirma {
         liste.add(8);
         liste.add(6);
         liste.add(9);
-        System.out.println(liste); // [5, 7, 8, 6, 9]
-
+        // once iterator u olusturup, sona yollayalim
         ListIterator itr=liste.listIterator();
         while(itr.hasNext()){
             itr.next();
         }
-
         while(itr.hasPrevious()){
-            System.out.print(itr.previous()+" ");
+            System.out.print(itr.previous() + " ");
         }
+
     }
 }
--- src/day46_iterator_collections/C04_IstenenElemanlariSilme.java
+++ src/day46_iterator_collections/C04_IstenenElemanlariSilme.java
@@ -6,6 +6,13 @@ import java.util.ListIterator;
 
 public class C04_IstenenElemanlariSilme {
     public static void main(String[] args) {
+       /*
+        Soru 3) Bir listedeki istenen sayi araliginda olmayan
+        elementleri silen bir program yaziniz …
+        (2. liste olusturmadan, gecerli liste uzerinde islem yapiniz)
+        Orn : [2,13,56,23,45,14,40] istenen aralik 20 ile 40 arasi (sinirlar dahil)
+        output: [23,40]
+         */
         List<Integer> liste= new ArrayList<>();
         liste.add(2);
         liste.add(13);
@@ -14,13 +21,10 @@ public class C04_IstenenElemanlariSilme {
         liste.add(45);
         liste.add(14);
         liste.add(40);
-
-        ListIterator itr = liste.listIterator();
-
-        while (itr.hasNext()) {
-
+        ListIterator itr =liste.listIterator();
+        while (itr.hasNext()){
             Object sayi=itr.next();
-            if ((Integer) sayi<20 || (Integer) sayi>40){
+            if ((Integer)sayi<20 || (Integer)sayi>40){
                 itr.remove();
             }
         }
