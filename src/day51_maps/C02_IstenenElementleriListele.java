fc254d2070e7696a365ed70e3138b2b9dfcf8d17
Adem Polat <polatadem.2541@gmail.com>
2022-04-23 01:32:25 +0200

    21.04.2022

---
+++ src/day51_maps/C02_IstenenElementleriListele.java
@@ -0,0 +1,27 @@
+package day51_maps;
+
+import day49_maps.MapOlustur;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class C02_IstenenElementleriListele {
+    /*
+    Soru 2 ) Verilen map’te istenen programlama dilini bilen kisileri list olarak donduren bir method yaziniz.
+ mapè { 101=Ali, Can, java, 102=Veli, Yan, java, 103=Ali, Yan, C#}
+ Istenen dil è java
+ Sonuc è [Ali, Veli]
+     */
+    public static void main(String[] args) {
+       Map<Integer, String>sinifMap= MapOlustur.myMap();
+       String istenenDil="java";
+       List<String> isteneDiliBilenlerListesi=istenenDiliBilenListesiOlustur(sinifMap,istenenDil);
+    }
+
+    public static List<String> istenenDiliBilenListesiOlustur(Map<Integer, String> sinifMap, String istenenDil) {
+        List<String> isteneDiliBilenlerListesi=new ArrayList<>();
+       // isteneDiliBilenlerListesi.addAll(sinifMap.values());
+        return null;
+    }
+}
